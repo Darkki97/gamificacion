642d1ac0e6f2307a92e3913ecbc203743de396cc
Francisco <1542057@alu.murciaeduca.es>
2018-10-08 09:14:45 +0200

    Creando metodo remoto getName de juego

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Juego) {
+/**
+ * Devuelve nombre de un juego
+ * @param {Function(Error, string)} callback
+ */
+
+Juego.prototype.getNombre = function(callback) {
+  var juego=this;
+  // TODO
+  callback(null, juego.nombre);
+};
 
 };
